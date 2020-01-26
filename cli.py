a8907d050f3f68d8de1689a23b0570c01dcd9383
Mohammadreza Shaghouzi <sh.mohammad66@gmail.com>
2020-01-26 12:15:33 +0330

    change regex

--- cli.py
+++ cli.py
@@ -14,41 +14,63 @@ class TransferSanitizer:
         '/': '&#x2F;'
     }
 
-    def __init__(self, entity: str = ''):
+    def __init__(self, url: str, entity: str = ''):
+        self.url = url
         self.entity: str = entity
 
     @staticmethod
-    def sanitize(entity: str = '') -> str:
+    def markdown_to_html(entity: str = '') -> str:
+        return markdown.markdown(entity)
+
+    @staticmethod
+    def escape(entity: str = '') -> str:
         import re
-        pattern = re.compile(r'\b(' + '|'.join(
-            [*TransferSanitizer.special_chars.values(), *TransferSanitizer.special_chars.keys()]) + r')\b')
+        pattern = re.compile(r'(' + '|'.join(
+            [*TransferSanitizer.special_chars.values(), *TransferSanitizer.special_chars.keys()]) + r')')
         return pattern.sub(lambda x: TransferSanitizer.special_chars[
             x.group()] if x.group() in TransferSanitizer.special_chars else x.group(), entity)
+
+    @staticmethod
+    def filter(escaped: str = '') -> str:
+        return escaped
+
+    @staticmethod
+    def validate(filtered: str = '') -> str:
+        return filtered
+
+    @staticmethod
+    def sanitize(entity: str = '') -> str:
+        escaped = TransferSanitizer.escape(entity)
+        filtered = TransferSanitizer.filter(escaped)
+        sanitized = TransferSanitizer.validate(filtered)
+        return sanitized
 
     @staticmethod
     def post_data(url: str, sanitized_entity: str) -> Dict[str, str]:
         if not url or not sanitized_entity:
             raise ValueError('url and sanitized_entity must be not null')
         return {
-            'status': 'ok'
+            'status': 'ok',
+            'sanitized': sanitized_entity
         }
 
     def send(self) -> Dict[str, str]:
-        return TransferSanitizer.post_data(TransferSanitizer.sanitize(self.entity))
+        return self.post_data(self.url, self.sanitize(self.markdown_to_html(self.entity)))
 
 
 if __name__ == '__main__':
-
-    html = markdown.markdown('<b>sal&am"dwd"\'')
-
-    print(TransferSanitizer.sanitize(html))
-    quit()
-
-    transfer = TransferSanitizer()
+    url = 'https://my-json-server.typicode.com/softrebel/levelUpTask1/entity'
+    transfer = TransferSanitizer(url)
     try:
         if len(sys.argv) > 1:
             transfer.entity = sys.argv[1]
         else:
             transfer.entity = input('Please Enter Your HTML/Markdown Body:\n')
+
+        print(transfer.send())
+
+    except ValueError as err:
+        raise ValueError('argument mode must be integer')
+
     except Exception as err:
-        raise Exception('Error on transferring body: ' + err)
+        raise Exception('Error on transferring body: {}'.format(err))
